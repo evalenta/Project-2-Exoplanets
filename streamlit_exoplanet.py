a8d71b4b582a41cafbb09b8e94c81bfd01b68011
itviolin2013 <isabelletardivon@yahoo.com>
2026-03-12 22:36:50 -0400

    Added basic list of exoplanets to choose from and had streamlit write something depending on the exoplanet chosen

--- streamlit_exoplanet.py
+++ streamlit_exoplanet.py
@@ -2,6 +2,10 @@ import streamlit as st
 # streamlit run streamlit_exoplanet.py
 st.title("Exoplanet")
 
+option = st.selectbox(
+    "Select an exoplanet:",
+    ("HD 189733B", "Kepler 17B", "Kepler 20F"),
+)
 
-
+st.write("You selected:", option)
 
